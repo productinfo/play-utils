4dc86c0e39c0f8559ffe8630e0225be7a619f8fe
Alison Clarke <aclarke@scottlogic.co.uk>
2014-06-24 11:59:20 +0100

    Added protocol for a view controller managed by the Play gallery.

---
+++ ShinobiPlayUtils/ShinobiPlayUtils/SPUGalleryManagedViewController.h
@@ -0,0 +1,15 @@
+//
+//  SPUGalleryManagedViewController.h
+//  ShinobiPlayUtils
+//
+//  Created by Alison Clarke on 24/06/2014.
+//  Copyright (c) 2014 Alison Clarke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol SPUGalleryManagedViewController <UIViewController>
+
+-(instancetype)initWithViewFrame:(CGRect)frame;
+
+@end
