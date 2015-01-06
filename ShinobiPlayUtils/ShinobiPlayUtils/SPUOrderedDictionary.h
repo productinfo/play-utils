384734badc5c2984ed5bc050095f6febb003ba15
Alison Clarke <aclarke@scottlogic.co.uk>
2015-01-06 10:10:14 +0000

    Added license to headers.

--- ShinobiPlayUtils/ShinobiPlayUtils/SPUOrderedDictionary.h
+++ ShinobiPlayUtils/ShinobiPlayUtils/SPUOrderedDictionary.h
@@ -1,9 +1,22 @@
 //
-//  OrderedDictionary.h
-//  ShinobiControlsTestApp
+//  SPUOrderedDictionary.h
+//  ShinobiPlayUtils
 //
-//  Created by  on 30/04/2012.
-//  Copyright (c) 2012 Scott Logic. All rights reserved.
+//  Created by Alison Clarke on 24/06/2014.
+//
+//  Copyright 2014 Scott Logic
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 
 @import Foundation;
